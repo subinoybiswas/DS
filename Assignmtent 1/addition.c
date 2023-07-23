3a8730ac0d8007fc9f656e6f224f80ee8c58e45e
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2023-07-23 15:29:12 +0530

    added

--- Assignmtent 1/addition.c
+++ Assignmtent 1/addition.c
@@ -1,20 +1,13 @@
 #include<stdio.h>
 
 int main(){
-    int m,n,m2,n2;
-    printf("\n\tINPUT\n------------------------------\n");
-    printf("Matrix1: Enter m,n: ");
+    int m,n;
+    printf("\n\tINPUT VALUES\n------------------------------\n");
+    printf("Matrix1 & Matrix 2: Enter m,n: ");
     scanf("%d %d",&m,&n);
-  
-    printf("Matrix2: Enter m,n: ");
-    scanf("%d %d",&m2,&n2);
 
-    if(n!=m2){
-        printf("Can't be multipled");
-    }
-    else{
     int matrix[m][n];
-    int matrix2[m2][n2];
+    int matrix2[m][n];
     for(int i=0;i<m;i++){
         printf("Matrix1: Enter Row %d: ",i);
         for(int j=0;j<n;j++){
@@ -22,28 +15,21 @@ int main(){
             scanf("%d",&matrix[i][j]);
         }
     }
-
-     for(int i=0;i<m2;i++){
+    printf("\n");
+     for(int i=0;i<m;i++){
         printf("Matrix2: Enter Row %d: ",i);
-        for(int j=0;j<n2;j++){
+        for(int j=0;j<n;j++){
             scanf("%d",&matrix2[i][j]);
         }
     }
     printf("\n\tANSWER\n------------------------------\n");
     for(int i=0;i<m;i++){
         printf("|");
-        for(int j=0;j<n2;j++){
-            int add=0;
-            for(int k=0;k<n;k++){
-                add+=matrix[i][k]*matrix2[k][j];
-            }
-            printf("% d ",add);
-            add=0;
+        for(int j=0;j<n;j++){
+            printf(" %d ",matrix[i][j]+matrix2[i][j]);
         }
         printf("|\n");
     }
-}
-
 
     return 0;
 }
